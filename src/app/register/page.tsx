d214aba7c3d94abcd2a306857894d959247f7f2a
Abdulloh PC <sabirovabdulloh2004@gmail.com>
2024-06-02 10:18:26 +0500

    ё1

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,30 +1,70 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ExpandMore } from '@mui/icons-material';
 import { Select, SelectItem } from "@nextui-org/react";
 // import BtnPurple from '@/components/core/AllComponent/buttonPurple';
-import { formInput } from './data'
 import TypeOfBusiness from '@/components/typeOfBisness';
+import axios from 'axios';
 
 
-export default function Home2() {
+export default function Registration() {
     const [isCollapse, setCollapse] = useState<boolean>(false);
-    const typeOfBisness = 'ООО'
-    const systemTax = 'УСН'
+    const [ipAddress, setIpAdress] = useState('')
     const [fillingStep, setFillingStep] = useState(1)
+    const [bisnessActivitiesList, setBisnessActivitiesList] = useState<any[]>([])
+    const [businessActivity, setBusinessActivity] = useState<string>('')
     const [typeOfBusiness, setTypeOfBusiness] = useState('')
     const [taxSystem, setTaxSystem] = useState('')
     const [username, setUsername] = useState('')
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [patronimic, setPatronimic] = useState('')
     const [email, setEmail] = useState('')
     const [phone, setPhone] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
 
 
-    const handleSubmit = () => {
-        console.log('Зарегистрировался')
+    useEffect(() => {
+        axios.get('/api/activities').then((res: any) => {
+            setBisnessActivitiesList(res.data.data)
+        })
+        axios.get('https://api.ipify.org?format=json').then((res: any) => {
+            setIpAdress(res.data.ip)
+        })
+    }, [])
+
+    function setOrgType(type: string) {
+        setTypeOfBusiness(type)
+        setFillingStep(2)
+    }
+
+    function setOrgTaxSys(sys: string) {
+        setTaxSystem(sys)
+        setFillingStep(3)
+    }
+
+    const handleSubmit = (e: Event) => {
+        e.preventDefault()
+        axios.post('/api/register', {
+            username: email.substring(0, 5) + phone.substring(5, 9),
+            name: name.split(' ')[1],
+            surname: name.split(' ')[0],
+            patronimic: name.split(' ')[2] || '',
+            email,
+            phone,
+            code_phrase: JSON.stringify([]),
+            device: JSON.stringify({
+                name: navigator?.appName,
+                ip: ipAddress
+            }),
+            status: 2,
+            password
+        }).then((res: any) => {
+            localStorage.setItem(btoa('token'), res.data.token)
+        })
     }
 
     const btnClass = {
@@ -34,9 +74,9 @@ export default function Home2() {
 
     return (
         <main className='w-full flex justify-center items-center min-h-screen'>
-            <div className="w-[600px] min-h-[750px] shadow-2xl px-[113px] flex flex-col items-center">
-                <Image className='mb-[17px] mt-[-85px]' src='/iconMenu/logoA.svg' width={173} height={120} alt="LogoAuth" />
-                <div className='flex flex-col'>
+            <div className="lg:w-[600px] w-full min-h-[750px] lg:shadow-2xl shadow-none px-[115px] py-[30px] flex flex-col items-center">
+                <Image className='mb-[17px] lg:mt-[-85px] mt-[50px]' src='/iconMenu/logoA.svg' width={173} height={120} alt="LogoAuth" />
+                <div className='flex flex-col items-center'>
                     <div className='text-center font-montserrat'>
                         <p className='text-[36px] font-bold text-purpleLg '>Мухосиби Ман</p>
                         <p className='text-[22px] font-bold text-purpleMid'>Осон, Кулай, Тез</p>
@@ -45,7 +85,7 @@ export default function Home2() {
                         <p className={fillingStep == 1 ? 'text-[24px]' : 'text-[18px] mb-[10px]'}>Выберите вид деятельности
                             {fillingStep == 1
                                 ? (<></>)
-                                : (<span className='text-purpleMid underline pl-[10px]'>{typeOfBisness}</span>)
+                                : (<span className='text-purpleMid underline pl-[10px]' onClick={() => setFillingStep(1)}>{typeOfBusiness}</span>)
                             }
                         </p>
                         {fillingStep < 2
@@ -53,46 +93,82 @@ export default function Home2() {
                             : (<p className={fillingStep < 3 ? 'text-[24px]' : 'text-[18px] mb-[10px]'}>Система налогообложения
                                 {fillingStep < 3
                                     ? (<></>)
-                                    : (<span className='text-purpleMid underline pl-[10px]'>{systemTax}</span>)
+                                    : (<span className='text-purpleMid underline pl-[10px]' onClick={() => setFillingStep(2)}>{taxSystem}</span>)
                                 }
                             </p>)
 
                         }
 
                         <div className={(fillingStep == 1) ? 'flex text-purpleLg justify-between text-[72px] mt-[25px] transition' : 'flex text-purpleLg justify-between text-[72px] mt-[25px] transition pointer-events-none opacity-0 z-[-1] h-0'}>
-                            <TypeOfBusiness name1='ИП' name2='ООО' />
+                            <h4 onClick={() => setOrgType('ИП')} className='underline cursor-pointer'>ИП</h4>
+                            <span className='text-[#CCCCCC] text-[60px] mt-3 mx-5'>|</span>
+                            <h4 onClick={() => setOrgType('ООО')} className='underline cursor-pointer'>ООО</h4>
                         </div>
 
                         <div className={(fillingStep == 2) ? 'flex text-purpleLg justify-between text-[72px] mt-[25px] transition' : 'flex text-purpleLg justify-between text-[72px] mt-[25px] transition pointer-events-none opacity-0 z-[-1] h-0'}>
-                            <TypeOfBusiness name1='УСН' name2='УСН' name3='минус расходы' />
+                            <h4 onClick={() => setOrgTaxSys('УСН доходы')} className='underline cursor-pointer'>УСН</h4>
+                            <span className='text-[#CCCCCC] text-[60px] mt-3 mx-5'>|</span>
+                            <h4 onClick={() => setOrgTaxSys('УСН доходы минус расходы')} className='underline cursor-pointer flex flex-col'>
+                                <span className='underline'>УСН</span>
+                                <span className='text-[23px] mt-[-20px] no-underline'>минус расходы</span>
+                            </h4>
                         </div>
                     </div>
-                    <form onSubmit={handleSubmit} className={(fillingStep == 3) ? 'w-[374px] mt-[21px] transition' : 'w-[374px] mt-[21px] transition pointer-events-none opacity-0 z-[-1] h-0'}>
+                    <form onSubmit={(e: any) => handleSubmit(e)} className={(fillingStep == 3) ? 'w-[374px] transition' : 'w-[374px]  transition pointer-events-none opacity-0 z-[-1] h-0'}>
                         <div className='flex flex-col'>
-                            {formInput.map((o) => (
-                                <React.Fragment key={o.id}>
-                                    {(o.id === 2) ? (
-                                        <>
-                                            <Select
-                                                placeholder="Выберите тип деятельности"
-                                                labelPlacement="outside"
-                                                className="mt-[20px] pl-[5px] bg-[#F1F1F1] border-b-2 text-[14px]"
-                                                disableSelectorIconRotation
-                                                classNames={btnClass}
-                                                selectorIcon={<ExpandMore />}
-                                            >
-                                                {o.typeOfBisness ? o.typeOfBisness.map((item) => (
-                                                    <SelectItem key={item.txt} value={item.txt}>
-                                                        {item.txt}
-                                                    </SelectItem>
-                                                )) : (<></>)}
-                                            </Select>
-                                        </>
-                                    ) : (
-                                        <input key={o.id} type={o.type} placeholder={o.placeHolder} className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' />
-                                    )}
-                                </React.Fragment>
-                            ))}
+                            <input 
+                                type="text" 
+                                placeholder="ФИО" 
+                                value={name} 
+                                onChange={(e) => setName(e.target.value)} 
+                                className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' 
+                            />
+                            <Select
+                                placeholder="Выберите тип деятельности"
+                                labelPlacement="outside"
+                                className="mt-[20px] pl-[5px] bg-[#F1F1F1] border-b-2 text-[14px]"
+                                disableSelectorIconRotation
+                                classNames={btnClass}
+                                selectedKeys={businessActivity}
+                                onChange={(e) => setBusinessActivity(e.target.value)}
+                                selectorIcon={<ExpandMore />}
+                            >
+                                {bisnessActivitiesList.map((item) => (
+                                    <SelectItem key={item.id} value={item.id}>
+                                        {item.title}
+                                    </SelectItem>
+                                ))}
+                            </Select>
+                            <input 
+                                type="text" 
+                                placeholder="Email" 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)} 
+                                className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' 
+                            />
+                            
+                            <input 
+                                type="text" 
+                                placeholder="Номер телефона" 
+                                value={phone} 
+                                onChange={(e) => setPhone(e.target.value)} 
+                                className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' 
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Пароль" 
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)} 
+                                className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' 
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Подтверждения пароля" 
+                                value={passwordConfirmation} 
+                                onChange={(e) => setPasswordConfirmation(e.target.value)} 
+                                className='border-b-2 h-[30px] bg-[#F1F1F1] mt-[20px] pl-[5px] text-[14px] hover:border-b-2  focus:outline-none' 
+                            />
+                            
                             <button className='bg-purple-600 rounded text-white p-1 mt-[20px]'>Зарегистрироваться</button>
                         </div>
 
